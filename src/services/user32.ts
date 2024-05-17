27a35b576174c69d2ab4adcc4715740743922eb9
Roma Zvarych (hisbvdis) <hisbvdis@gmail.com>
2024-05-17 20:43:46 +0300

    new user32 methods

--- src/services/user32.ts
+++ src/services/user32.ts
@@ -4,6 +4,12 @@ const user32 = koffi.load("user32.dll");
 export const GetForegroundWindow: () => number = user32.func("GetForegroundWindow", "int", [] );
 export const SetForegroundWindow: (hwnd:number) => boolean = user32.func("SetForegroundWindow", "bool", ["int"] );
 export const GetShellWindow: () => number = user32.func("GetShellWindow", "int", [] );
+export const GetWindow: (hwnd:number, uCmd:number) => number = user32.func("GetWindow", "int", ["int", "int"] );
 
 const EnumWindowsProc = koffi.proto("__stdcall", "EnumWindowsProc", "bool", ["int", "int"]);
 export const EnumWindows = user32.func("__stdcall", "EnumWindows", "bool", [koffi.pointer(EnumWindowsProc), "int"]);
+
+export const hasOwnerWindow: (hwnd:number) => boolean = (hwnd) => Boolean(GetWindow(hwnd, 6));
+export const getOwnerWindow: (hwnd:number) => number = (hwnd) => GetWindow(hwnd, 6);
+export const hasPopupWindow = (hwnd:number):boolean => Boolean(GetWindow(hwnd, 6));
+export const getPopupWindow = (hwnd:number):number => GetWindow(hwnd, 6);
